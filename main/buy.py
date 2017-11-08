e833224e68a14e54a5c9d2c78e2c418167176ce6
jinjiawu <734561145@qq.com>
2017-11-08 22:36:45 +0800

    commit

--- main/buy.py
+++ main/buy.py
@@ -20,8 +20,7 @@ class buy_goods():
 		for i in xrange(len(item_list)):
 			item_names[i+1] = item_list[i][1]#获取物品名字
 			item_price[i+1] = int(item_list[i][2])#获取物品价格
-		print(item_names)
-		return item_price
+		return item_names,item_price
 
 	def get_user_account(self):
 		'''获取用户帐号信息'''
@@ -59,28 +58,54 @@ class buy_goods():
 			else:
 				print(u'用户名或者密码出错，请重新登录！')
 
+	def chose_good(self,num,all_items,all_price,last_money):
+		good_price = all_price[num]
+		last_money = int(self.get_user_money())
+		if good_price <= last_money:
+			last_money -= good_price
+			write_to_excel(last_money,'user_money.xlsx')
+			write_to_excel(all_items[num],'user_items.xlsx')#将购买的商品写入到表格中
+			print(u'你购买了:%s'%all_items[num])
+			print(u'花费了：%d'%good_price)
+			print('还剩:%d'%last_money)
+		if good_price > last_money:
+			print('不好意思，钱不够买该商品')
+
 	def buy_goods(self):
 		'''购买商品'''
-		last_money = int(self.get_user_money())
-		goos_price = self.get_all_items()#这里返回的是字典
+		all_items,all_price = self.get_all_items()#这里返回的是字典
 		while True:
+			last_money = int(self.get_user_money())#获取剩余的钱
 			print(u'如果想要退出，就输入q')
 			print(u'输入物品编号来购买物品')
 			your_input = raw_input('please input your code:')
 			if your_input == 'q':
-				#将剩余的钱写入到表格中
+				#将剩余的钱写入到表格中,之后再打印出来
 				write_to_excel(last_money,'user_money.xlsx')
+				print(u'还剩:%d'%self.get_user_money())
+				print(u'你已购买物品如下:%s'%self.get_user_items())
 				break
+			if your_input == 1:
+				self.chose_good(1)
+			if your_input == 2:
+				self.chose_good(2)
+			if your_input == 3:
+				self.chose_good(3)
+			if your_input == 4:
+				self.chose_good(4)
+			if your_input == 5:
+				self.chose_good(5)
+			if your_input == 6:
+				self.chose_good(6)
 
-
-
-	
 
 
 	def main(self):
 		print('你现在有金钱:%d'%(self.get_user_money()))
 		print('有如下商品:')
-		self.get_all_items()
+		all_items,all_price = self.get_all_items()
+		last_money = int(self.get_user_money())
+		self.chose_good(6,all_items,all_price,last_money)
 
 
 buy_goods().main()
